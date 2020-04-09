f0b966638d747ae367d1e57771a65fefeb769bd0
eboshare <t.lila@list.ru>
2020-04-10 01:24:10 +0400

    Add a inline and reply keyboard to helper

--- tgstarter/utils/helper.py
+++ tgstarter/utils/helper.py
@@ -1,8 +1,15 @@
-from typing import Any, List, Callable
+from typing import (
+    Any,
+    List,
+    Callable,
+    Union,
+    Dict,
+)
 from enum import Enum
 from textwrap import dedent
 
 import jinja2
+from aiogram import types
 
 
 def function_fullname(function: Callable[..., Any]) -> str:
@@ -14,6 +21,50 @@ def get_template_function(jinja2_env: jinja2.Environment) -> Callable[..., Any]:
         return jinja2_env.from_string(source=dedent(source))
 
     return template
+
+
+def button_from_source(button: Union[str, Dict[str, Any]]) -> types.KeyboardButton:
+    if isinstance(button, str):
+        return types.KeyboardButton(text=button)
+    elif isinstance(button, types.KeyboardButton):
+        return button
+    else:
+        assert False
+
+
+def ReplyKeyboardMarkup(
+    *source: Union[str, types.KeyboardButton, List[Union[str, types.KeyboardButton]]],
+    resize_keyboard: bool = True,
+    **kwargs: Any
+) -> types.ReplyKeyboardMarkup:
+    reply_markup = types.ReplyKeyboardMarkup(resize_keyboard=resize_keyboard, **kwargs)
+    for item in source:
+        if isinstance(item, list):
+            reply_markup.row(
+                *[
+                    button_from_source(button) for button in item
+                ]
+            )
+        else:
+            reply_markup.add(button_from_source(item))
+
+    return reply_markup
+
+
+def InlineKeyboardMarkup(
+    *source: Union[types.InlineKeyboardButton, List[types.InlineKeyboardButton]],
+    **kwargs: Any
+) -> types.InlineKeyboardMarkup:
+    inline_keyboard = types.InlineKeyboardMarkup(**kwargs)
+    for item in source:
+        if isinstance(item, list):
+            inline_keyboard.row(*item)
+        elif isinstance(item, types.InlineKeyboardButton):
+            inline_keyboard.add(item)
+        else:
+            assert False
+
+    return inline_keyboard
 
 
 class Item:
