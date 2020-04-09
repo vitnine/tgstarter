f7e957a6e3bea2072dcac5df0222b1136acb0d0b
eboshare <t.lila@list.ru>
2020-04-09 22:40:37 +0400

    Add custom yaml tags for keyboards

--- tgstarter/utils/yaml_tools.py
+++ tgstarter/utils/yaml_tools.py
@@ -2,11 +2,13 @@ from typing import (
     Optional,
     Dict,
     Any,
-    Callable,
+    Callable, Union,
 )
+import json
 
 import yaml
 import jinja2
+from aiogram import types
 
 
 def load_yamls(*paths: str, loader: yaml.Loader) -> Dict[str, Any]:
@@ -27,3 +29,72 @@ def get_template_constructor(jinja2_env: Optional[jinja2.Environment] = None) ->
             return jinja2.Template(source=node.value)
 
     return template_constructor
+
+
+def button_from_source(button: Union[str, Dict[str, Any]]) -> types.KeyboardButton:
+    if isinstance(button, str):
+        return types.KeyboardButton(text=button)
+    elif isinstance(button, dict):
+        return types.KeyboardButton(**button)
+    else:
+        assert False
+
+
+def reply_markup_constructor(loader: yaml.Loader, node: yaml.Node) -> types.ReplyKeyboardMarkup:
+    reply_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    source = loader.construct_sequence(node=node, deep=True)
+    for item in source:
+        if isinstance(item, list):
+            reply_markup.row(
+                *[
+                    button_from_source(button) for button in item
+                ]
+            )
+        elif isinstance(item, dict):
+            reply_markup.add(types.KeyboardButton(**item))
+        elif isinstance(item, str):
+            reply_markup.add(types.KeyboardButton(text=item))
+        else:
+            assert False
+
+    return reply_markup
+
+
+def get_callback_string_constructor(max_bytes: int = 64) -> Callable[..., Any]:
+
+    def callback_string_constructor(loader: yaml.Loader, node: yaml.Node) -> str:
+        value = node.value
+        if not isinstance(value, str):
+            value = json.dumps(
+                obj=node.value,
+                ensure_ascii=False,
+                separators=(',', ':'),
+                default=str,
+            )
+
+        length = len(value.encode())
+        if length > max_bytes:
+            raise ValueError(f'callback of {value} is too long, {length} > {max_bytes}')
+        else:
+            return value
+
+    return callback_string_constructor
+
+
+def inline_keyboard_constructor(loader: yaml.Loader, node: yaml.Node) -> types.ReplyKeyboardMarkup:
+    inline_keyboard = types.InlineKeyboardMarkup()
+    source = loader.construct_sequence(node=node, deep=True)
+    for item in source:
+        if isinstance(item, list):
+            inline_keyboard.row(
+                *[
+                    types.InlineKeyboardButton(**button)
+                    for button in item
+                ]
+            )
+        elif isinstance(item, dict):
+            inline_keyboard.add(types.InlineKeyboardButton(**item))
+        else:
+            assert False
+
+    return inline_keyboard
