84dd9c4759b4082f0faa534db843b54f003a7656
eboshare <t.lila@list.ru>
2020-04-13 02:35:54 +0400

    Add a quick Bot import

--- tgstarter/__init__.py
+++ tgstarter/__init__.py
@@ -1,11 +1,13 @@
 from .dispatcher.dispatcher import Dispatcher
 from .storage.mongo_storage import MongoStorage
+from .bot.bot import Bot
 
 
 __all__ = (
     '__version__'
     'Dispatcher',
     'MongoStorage',
+    'Bot',
 )
 
 __version__ = '0.1.0'
