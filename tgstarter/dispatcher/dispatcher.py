8e866df12f3ed4f9ae2547ee4a41cf5d76107067
eboshare <t.lila@list.ru>
2020-07-16 21:40:54 +0400

    Bring back the any_update_handler

--- tgstarter/dispatcher/dispatcher.py
+++ tgstarter/dispatcher/dispatcher.py
@@ -1,5 +1,14 @@
+from typing import Callable
+
 import aiogram
+
+from tgstarter.utils.typing import AsyncCallbackVar
 
 
 class Dispatcher(aiogram.Dispatcher):
-    pass
+    def any_update_handler(self) -> Callable[[AsyncCallbackVar], AsyncCallbackVar]:
+        def wrapper(callback: AsyncCallbackVar) -> AsyncCallbackVar:
+            self.updates_handler.register(callback, index=0)
+            return callback
+
+        return wrapper
